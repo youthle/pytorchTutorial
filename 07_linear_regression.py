6521503002425152aa21ba8f9cfcca81f190b0e4
youthle <49577873+youthle@users.noreply.github.com>
2021-12-30 12:22:21 -0600

    Update 07_linear_regression.py

--- 07_linear_regression.py
+++ 07_linear_regression.py
@@ -1,16 +1,19 @@
 import torch
 import torch.nn as nn
-import numpy as np
-from sklearn import datasets
-import matplotlib.pyplot as plt
+import numpy as np  # for data transformation
+from sklearn import datasets # get some data
+import matplotlib.pyplot as plt  # plot data
+#import scipy
+
+#print(scipy.__version__)
 
 # 0) Prepare data
 X_numpy, y_numpy = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
 
 # cast to float Tensor
-X = torch.from_numpy(X_numpy.astype(np.float32))
+X = torch.from_numpy(X_numpy.astype(np.float32)) #原本的是double型
 y = torch.from_numpy(y_numpy.astype(np.float32))
-y = y.view(y.shape[0], 1)
+y = y.view(y.shape[0], 1) #把原数据整成一列，每行为单独的output
 
 n_samples, n_features = X.shape
 
